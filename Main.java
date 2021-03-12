2de030621537349c7c3352d946899d9abbcf4b52
Lexx-Psi <60262142+Lexx-Psi@users.noreply.github.com>
2021-03-12 09:39:04 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cash = 50;
+        int percent = 5;
+        int miles = (cash * percent) / 100;
+        System.out.println(miles);
+    }
+}
